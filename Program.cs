ef28ec164c7e5709827ba79d0ee992b2f6681f6e
pedrosantos57 <43537290+pedrosantos57@users.noreply.github.com>
2018-10-15 12:10:24 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,7 +10,37 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            //Coloque aqui o seu codigo
+            Console.WriteLine("Insira a Abcissa");
+            int abcissa = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Insira a Ordenada");
+            int ordenada = Convert.ToInt32(Console.ReadLine());
+
+            if (abcissa > 0 && ordenada > 0)
+            {
+                Console.WriteLine("Esta no primeiro quadrante!");
+            }
+            else
+            {
+                if (abcissa < 0 && ordenada > 0)
+                {
+                    Console.WriteLine("Esta no segundo quadrante!");
+                }
+                else
+                {
+                    if (abcissa < 0 && ordenada < 0)
+                    {
+                        Console.WriteLine("Esta no terceiro quadrante!");
+                    }
+                    else
+                    {
+                        if (abcissa > 0 && ordenada < 0)
+                        {
+                            Console.WriteLine("Esta no quarto quadrante!");
+                        }
+                    }
+                }
+            }
+            Console.ReadLine();
         
         }
     }
